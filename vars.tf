da3196d4e92a429938dd15d45afd4fa38827b3d7
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-04-11 14:36:57 +0200

    update

--- vars.tf
+++ vars.tf
@@ -2,7 +2,10 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "region" {}
 variable "instance_type" {}
-variable "key_name" {}
+
+variable "key_name" {
+  description = "uh?"
+}
+
 variable "private_key" {}
 variable "ami" {}
-
