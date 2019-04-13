dad2e9d27782ad8383268fe9842f244099908a78
Georgi Berchev <g.berchev@gmail.com>
2019-04-13 22:32:49 +0300

    repairing module configuration in order to make it works

--- example/vars.tf
+++ example/vars.tf
@@ -1,7 +1,27 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "region" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "private_key" {}
-variable "ami" {}
+variable "aws_access_key" {
+  description = "Your access key to AWS"
+}
+
+variable "aws_secret_key" {
+  description = "Your secret key to AWS"
+}
+
+variable "region" {
+  description = "Desired AWS region"
+}
+
+variable "instance_type" {
+  description = "Desired AWS instance type - t2.micro, t2.large, etc"
+}
+
+variable "ssh_key_name" {
+  description = "Name of your AWS ssh key pair, without .pem"
+}
+
+variable "private_key" {
+  description = "Full path to your AWS private key"
+}
+
+variable "ami" {
+  description = "Desired AMI from AWS, depending on region"
+}
